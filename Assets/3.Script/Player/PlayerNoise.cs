49d487aba1b84d945d20e54be9c5ea07c377b182
1Q <bsc6260@gmail.com>
2025-12-18 17:05:03 +0900

    1218 NoiseRefactoring
    update PlayerNoise.cs

--- Assets/3.Script/Player/PlayerNoise.cs
+++ Assets/3.Script/Player/PlayerNoise.cs
@@ -16,6 +16,9 @@ public class PlayerNoise : MonoBehaviour
     [Tooltip("초당 소음 감소 속도")]
     public float noiseDecaySpeed = 5.0f;
 
+    // 코루틴 제어용 변수
+    private Coroutine runningCoroutine;
+
     private void Update()
     {
         // 노이즈는 지속적으로 감소
@@ -29,10 +32,50 @@ public class PlayerNoise : MonoBehaviour
     }
 
     // 애니메이션 이벤트에서 이 함수를 호출합니다.
-    public void MakeNoise(float currNoise)
+    public void MakeNoiseOnce(float amount)
     {
-        noise = currNoise;
+        // 기존 노이즈보다 작은 값의 노이즈 발생 시 무시됨
+        noise = Mathf.Max(noise, amount);
         // 내 주변 radius 반경 내의 적들을 모두 찾음
+        FindEnemys(amount);
+    }
+
+    private IEnumerator MakeNoise_co(float amount, float interval)
+    {
+        while (true) // 무한 반복
+        {
+            noise = Mathf.Max(noise, amount);
+            FindEnemys(amount);   // 적 감지
+
+            // 인터벌만큼 대기 (이 시간 동안 Update에서 noise는 줄어듦)
+            yield return new WaitForSeconds(interval);
+        }
+    }
+
+    // 외부에서 호출할 땐 이 함수를 사용 (중복 방지 로직 포함)
+    public void StartNoiseCoroutine(float amount, float interval)
+    {
+        // 이미 돌고 있는 코루틴이 있다면 끄고 시작
+        StopNoiseCoroutine();
+
+        // 코루틴 시작 후 변수에 저장
+        runningCoroutine = StartCoroutine(MakeNoise_co(amount, interval));
+    }
+
+    // 멈출 때 호출할 함수
+    public void StopNoiseCoroutine()
+    {
+        // 이미 돌고있는 코루틴이 있는지 확인
+        if (runningCoroutine != null)
+        {
+            StopCoroutine(runningCoroutine);
+            // 코루틴 변수 초기화
+            runningCoroutine = null;
+        }
+    }
+
+    public void FindEnemys(float currNoise)
+    {
         Collider[] enemies = Physics.OverlapSphere(transform.position, currNoise, enemyLayer);
 
         foreach (Collider enemyCol in enemies)
