8e91c451e758a259d155e896c3f08a641b232102
Under_Star <pktom2@naver.com>
2025-12-18 15:01:49 +0900

    [1218 Merge 중입니다]

--- Assets/3.Script/Enemy/CaughtManager.cs
+++ Assets/3.Script/Enemy/CaughtManager.cs
@@ -14,11 +14,12 @@ public class CaughtManager : MonoBehaviour
     [Header("게이지 설정")]
     public float maxGauge = 100f;
     public float drainSpeed = 10f;
-    private float currentCauge = 0f;
+    private float currentGauge = 0f;
 
     private void Awake()
     {
-        if(instance == null)
+        // 싱글톤 구조
+        if (instance == null)
         {
             instance = this;
             DontDestroyOnLoad(gameObject);
@@ -31,31 +32,35 @@ public class CaughtManager : MonoBehaviour
 
     private void Update()
     {
-        if(currentCauge > 0)
+        // 게이지는 지속적으로 감소
+        if (currentGauge > 0)
         {
-            currentCauge -= Time.deltaTime * drainSpeed;
+            currentGauge -= Time.deltaTime * drainSpeed;
         }
 
-        if(CaughtSilder != null)
+        // 슬라이더의 value값에 게이지 값을 넣어줌
+        if (CaughtSilder != null)
         {
-            CaughtSilder.value = currentCauge;
-            CaughtSilder.gameObject.SetActive(currentCauge > 0.1f);
+            CaughtSilder.value = currentGauge;
+            //게이지 값이 존재할 때만 슬라이더 표시
+            CaughtSilder.gameObject.SetActive(currentGauge > 0.1f);
         }
     }
 
     public void AddCaught(float amount)
     {
-        currentCauge += amount;
+        currentGauge += amount;
 
-        if(currentCauge >= maxGauge)
+        if (currentGauge >= maxGauge)
         {
-            currentCauge = maxGauge;
+            currentGauge = maxGauge;
             GameOver();
         }
     }
 
     void GameOver()
     {
+        //임시 게임오버
         Debug.Log("Game Over!");
         Time.timeScale = 0;
     }
