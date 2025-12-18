f88e6b167f7d94014198bb235f9c562905efbbec
Ingye0 <qkrqjarms99@gmail.com>
2025-12-18 17:52:58 +0900

    상호작용능력 + 문상호작용

--- Assets/10.Input/PlayerInputAction.cs
+++ Assets/10.Input/PlayerInputAction.cs
@@ -136,6 +136,15 @@ public partial class @PlayerInputAction: IInputActionCollection2, IDisposable
                     ""processors"": """",
                     ""interactions"": """",
                     ""initialStateCheck"": false
+                },
+                {
+                    ""name"": ""Interact"",
+                    ""type"": ""Button"",
+                    ""id"": ""ec1e2628-94bb-45ea-a641-ee831c7b19ee"",
+                    ""expectedControlType"": """",
+                    ""processors"": """",
+                    ""interactions"": """",
+                    ""initialStateCheck"": false
                 }
             ],
             ""bindings"": [
@@ -237,6 +246,17 @@ public partial class @PlayerInputAction: IInputActionCollection2, IDisposable
                     ""action"": ""DecoySkillThrow"",
                     ""isComposite"": false,
                     ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": """",
+                    ""id"": ""06ce508c-7259-4dde-9b44-f80fbe6e25c0"",
+                    ""path"": ""<Keyboard>/e"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": "";PC"",
+                    ""action"": ""Interact"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
                 }
             ]
         }
@@ -256,6 +276,7 @@ public partial class @PlayerInputAction: IInputActionCollection2, IDisposable
         m_Player_TransformSkill = m_Player.FindAction("TransformSkill", throwIfNotFound: true);
         m_Player_DecoySkillStart = m_Player.FindAction("DecoySkillStart", throwIfNotFound: true);
         m_Player_DecoySkillThrow = m_Player.FindAction("DecoySkillThrow", throwIfNotFound: true);
+        m_Player_Interact = m_Player.FindAction("Interact", throwIfNotFound: true);
     }
 
     ~@PlayerInputAction()
@@ -341,6 +362,7 @@ public partial class @PlayerInputAction: IInputActionCollection2, IDisposable
     private readonly InputAction m_Player_TransformSkill;
     private readonly InputAction m_Player_DecoySkillStart;
     private readonly InputAction m_Player_DecoySkillThrow;
+    private readonly InputAction m_Player_Interact;
     /// <summary>
     /// Provides access to input actions defined in input action map "Player".
     /// </summary>
@@ -372,6 +394,10 @@ public partial class @PlayerInputAction: IInputActionCollection2, IDisposable
         /// Provides access to the underlying input action "Player/DecoySkillThrow".
         /// </summary>
         public InputAction @DecoySkillThrow => m_Wrapper.m_Player_DecoySkillThrow;
+        /// <summary>
+        /// Provides access to the underlying input action "Player/Interact".
+        /// </summary>
+        public InputAction @Interact => m_Wrapper.m_Player_Interact;
         /// <summary>
         /// Provides access to the underlying input action map instance.
         /// </summary>
@@ -413,6 +439,9 @@ public partial class @PlayerInputAction: IInputActionCollection2, IDisposable
             @DecoySkillThrow.started += instance.OnDecoySkillThrow;
             @DecoySkillThrow.performed += instance.OnDecoySkillThrow;
             @DecoySkillThrow.canceled += instance.OnDecoySkillThrow;
+            @Interact.started += instance.OnInteract;
+            @Interact.performed += instance.OnInteract;
+            @Interact.canceled += instance.OnInteract;
         }
 
         /// <summary>
@@ -439,6 +468,9 @@ public partial class @PlayerInputAction: IInputActionCollection2, IDisposable
             @DecoySkillThrow.started -= instance.OnDecoySkillThrow;
             @DecoySkillThrow.performed -= instance.OnDecoySkillThrow;
             @DecoySkillThrow.canceled -= instance.OnDecoySkillThrow;
+            @Interact.started -= instance.OnInteract;
+            @Interact.performed -= instance.OnInteract;
+            @Interact.canceled -= instance.OnInteract;
         }
 
         /// <summary>
@@ -527,5 +559,12 @@ public partial class @PlayerInputAction: IInputActionCollection2, IDisposable
         /// <seealso cref="UnityEngine.InputSystem.InputAction.performed" />
         /// <seealso cref="UnityEngine.InputSystem.InputAction.canceled" />
         void OnDecoySkillThrow(InputAction.CallbackContext context);
+        /// <summary>
+        /// Method invoked when associated input action "Interact" is either <see cref="UnityEngine.InputSystem.InputAction.started" />, <see cref="UnityEngine.InputSystem.InputAction.performed" /> or <see cref="UnityEngine.InputSystem.InputAction.canceled" />.
+        /// </summary>
+        /// <seealso cref="UnityEngine.InputSystem.InputAction.started" />
+        /// <seealso cref="UnityEngine.InputSystem.InputAction.performed" />
+        /// <seealso cref="UnityEngine.InputSystem.InputAction.canceled" />
+        void OnInteract(InputAction.CallbackContext context);
     }
 }
