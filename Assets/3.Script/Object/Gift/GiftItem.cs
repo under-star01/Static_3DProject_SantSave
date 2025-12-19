db6b1f2bad2e72b39e2463653079202096af353c
Ingye0 <qkrqjarms99@gmail.com>
2025-12-19 14:41:45 +0900

    선물매니저

---
+++ Assets/3.Script/Object/Gift/GiftItem.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class GiftItem
+{
+    public GiftType giftType;
+    public string giftName;
+    public Sprite icon;
+    public GameObject prefab;
+}
---
+++ Assets/3.Script/Object/Gift/GiftType.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum GiftType
+{
+    RTX_5090 = 0,
+    Book = 1,
+    Ball = 2
+}
